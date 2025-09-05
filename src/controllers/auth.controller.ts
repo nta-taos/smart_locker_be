bfbc2f8b64e97705c41d2e1da7c56e3d605644f6
Nguyen Tuan Anh <tuanhaoggg@gmail.com>
2025-09-05 11:25:24 +0700

    feat(api-auth-register): add JWT token response after successful register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,6 +4,7 @@ import { injectable, inject } from 'inversify'
 
 import { SuccessMessages } from '@/common/constants/messages'
 import { ApiSuccess } from '@/common/responses'
+import { signToken } from '@/common/utils/jwt'
 import TYPES from '@/di/types'
 import { User } from '@/entities/user.model'
 import { AuthService } from '@/services/auth.service'
@@ -23,7 +24,12 @@ export class AuthController {
       const userData = { ...user }
       delete (userData as Partial<User>).password
 
-      return ApiSuccess.created(userData, SuccessMessages.USER_REGISTERED).send(res)
+      const token = signToken({
+        sub: user.id.toString(),
+        role: user.role
+      })
+
+      return ApiSuccess.created({ user: userData, token }, SuccessMessages.USER_REGISTERED).send(res)
     } catch (error: unknown) {
       next(error)
     }
